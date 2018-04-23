e38511cbc88115f172e1ec62186f7225a894369a
udelsux <thoesy2010@users.noreply.github.com>
2018-04-23 18:01:21 -0400

    aaaaa
    aaa

--- model/Animal.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public abstract class Animal extends FloatingObjs{
-	//TO DO :Please fill in the functions
-	public Animal(String n, int x, int y) {
-		super(n, x, y);
-		// TODO Auto-generated constructor stub
-	}
-}
