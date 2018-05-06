0107fd1635b976e9568b0269f8b2f7ff13bbcb12
udcymen <udcymen@udel.edu>
2018-05-05 22:49:34 -0400

    Unit Test
    Lab 10 JUnit Test

--- src/model/ModelWorld.java
+++ src/model/ModelWorld.java
@@ -10,12 +10,6 @@ public class ModelWorld {
 		this.lof = l;
 	}//constructor
 	
-	public void updateWorld(){
-		for(FloatingObjs f : lof){
-			f.move();
-		}
-	}
-	
 	public ArrayList<FloatingObjs> getListOfFloatingObjs(){
 		return lof;
 	}
@@ -45,17 +39,5 @@ public class ModelWorld {
 	public void destory(){}
 	
 	public void spawn(){}
-
-	public static void main(String[] args){
-		ArrayList<FloatingObjs> loFloating = new ArrayList<FloatingObjs>();
-		loFloating.add(new ProtectedSpecies("bogturtle", 4 , 5));
-		loFloating.add(new InvasiveSpecies("bass", 4 , 5));
-		loFloating.add(new InvasiveSpecies("catFish", 4 , 5));
-		loFloating.add(new ProtectedSpecies("horseshoeCrab", 4 , 5));
-		
-		ModelWorld world = new ModelWorld(loFloating);
-		
-		System.out.println(world);
-	}//main
 
 }
