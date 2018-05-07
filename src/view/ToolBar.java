42abdefaf66331174b14111a79b30748bb8ad99e
Muhan Yu <yumh@udel.edu>
2018-05-07 15:18:44 -0400

    update Toolbar

--- src/view/ToolBar.java
+++ src/view/ToolBar.java
@@ -1,56 +1,86 @@
 package view;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import view.TimeCounter.FishTimer;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 
 public class ToolBar extends JPanel {
-	public String tool = "Invasion";
+	private JRadioButton invasiveTool;
+	private JRadioButton protectedTool;
+	private JRadioButton litterTool;
+	//private Timer myTimer;
 	
 	public ToolBar(){
-		//GridLayout g = new GridLayout(4,0);
-		//this.setLayout(g);
+		GridLayout g = new GridLayout(5,0);
+		this.setLayout(g);
 		setBtn();
+		
+/*		this.myTimer=new Timer(1000, new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				 while(time<120){
+				 time++;
+			     repaint();
+			    }
+				 timer.stop();
+		}
+		});*/
 		
 		ImageIcon icon = new ImageIcon(createImages("Dialogue2"));
 		JButton scoreBtn = new JButton("Score: 0");
-		scoreBtn.setBounds(0,780,260,260);
+		JButton timeBtn = new JButton("Time: 0:0");
 		scoreBtn.setFont(new Font("SansSerif",Font.ITALIC ,48));
-		scoreBtn.setEnabled(false);
+		timeBtn.setFont(new Font("SansSerif",Font.BOLD, 22));
+		//scoreBtn.setEnabled(false);
+		scoreBtn.setContentAreaFilled(false); 
+		scoreBtn.setBorderPainted(false); 
+		timeBtn.setContentAreaFilled(false); 
+		timeBtn.setBorderPainted(false); 
 		this.add(scoreBtn);
+		this.add(timeBtn);
+		this.setBackground(new Color(100,255,255));
 	}//constructor
 	
+
+
+	
+	
 	private void setBtn(){
-		setLayout(null);
 		ImageIcon icon = new ImageIcon(createImages("net"));
-		JRadioButton invasiveTool = new JRadioButton("", icon);
+		invasiveTool = new JRadioButton("Invasion", icon);
 		invasiveTool.setFont(new Font("SansSerif",Font.BOLD, 22));
-		invasiveTool.setBounds(0,0,260,260);
+		invasiveTool.setActionCommand("Invasion");//prime num used to find the species
+		invasiveTool.setContentAreaFilled(false); 
+		invasiveTool.setBorderPainted(false); 
 		
 		icon = new ImageIcon(createImages("camera"));
-		JRadioButton protectedTool = new JRadioButton("", icon);
+		protectedTool = new JRadioButton("Protected", icon);
 		protectedTool.setFont(new Font("SansSerif",Font.BOLD, 22));
-		protectedTool.setBounds(0,260,260,260);
+		protectedTool.setActionCommand("Protected");//prime num used to find the species
+		protectedTool.setContentAreaFilled(false); 
+		protectedTool.setBorderPainted(false); 
 		
 		icon = new ImageIcon(createImages("trashpicker"));
-		JRadioButton litterTool = new JRadioButton("", icon);
+		litterTool = new JRadioButton("Litter", icon);
 		litterTool.setFont(new Font("SansSerif",Font.BOLD, 22));
-		litterTool.setBounds(0,520,260,260);
+		litterTool.setActionCommand("Litter");//prime num used to find the species		
+		litterTool.setContentAreaFilled(false); 
+		litterTool.setBorderPainted(false); 
+		
 		ButtonGroup bp = new ButtonGroup();
 		bp.add(invasiveTool);
 		bp.add(protectedTool);
@@ -76,11 +106,21 @@ public class ToolBar extends JPanel {
 		JButton label = ((JButton) this.getComponent(3));
 		label.setText("Score: "+score);
 	}
+	public void updateTime(int time){
+		JButton label= ((JButton) this.getComponent(4));
+		label.setText("Time:"+time/60+":"+time%60);
+	}
+	
+	public void setActionListener(ActionListener tAL){
+		invasiveTool.addActionListener(tAL);
+		protectedTool.addActionListener(tAL);
+		litterTool.addActionListener(tAL);
+	}
 	
 	public static void main(String[] args){
 		JFrame frame = new JFrame();
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1920, 1080);
+		frame.setSize(320, 960);
 		ToolBar tb = new ToolBar();
 		frame.getContentPane().add(tb);
 		frame.setVisible(true);
