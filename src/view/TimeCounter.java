5d7da62a373b8f049f364ff0af2b4997e0a91341
Muhan Yu <yumh@udel.edu>
2018-05-07 03:59:19 -0400

    modified TimeCounter only

--- src/view/TimeCounter.java
+++ src/view/TimeCounter.java
@@ -1,44 +1,37 @@
 package view;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class TimeCounter extends JPanel{
 	private Timer timer;
-	private int minute;
-	private int second;
+	private int time;
+	private JButton timeBtn;
 	public TimeCounter(){
 		this.timer= new Timer(1000, new FishTimer());
+		timeBtn.setText("Time:0:00");
 	}
 	public void paintComponent(Graphics g){
+		timer.start();
+
 		//paint stuff
 		g.setFont(new Font("SansSerif",Font.BOLD, 22));
 		g.setColor(Color.WHITE);
-		super.paintComponent(g);
-		timer.start();
+		timeBtn.setText("Time:"+time/60+":"+time%60);
+
 		//operation
-		g.drawString(minute+":"+second, 1660, 1015);
-		timer.stop();
+
 	}
 	class FishTimer implements ActionListener{
 
 		 public void actionPerformed(ActionEvent e)
 		    {
 			 //game lasts no more than 2 minutes
-			 while(minute<2){
-			 second++;
-			 minute=second%60;}
+			 while(time<120){
+			 time++;
 		     repaint();
 		    }
-	}
-	public static void main(String[] args){
-		JFrame frame = new JFrame();
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(320, 960);
-		TimeCounter tc = new TimeCounter();
-		frame.getContentPane().add(tc);
-		frame.setVisible(true);
-	}//main
-	
+				timer.stop();
+		    }
+}
 }
