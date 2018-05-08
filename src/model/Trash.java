94bab875f6a93e1d39c8b4dca77bbc57169a2cad
ZhangQingsen <1463769805zqs@gmail.com>
2018-05-07 21:11:44 -0400

    done javadoc

--- src/model/Trash.java
+++ src/model/Trash.java
@@ -1,25 +1,59 @@
 package model;
 
+/**
+ * Class Trash
+ * @author Team 11 - 8
+ * */
+
 public class Trash extends FloatingObjs {
 	//subclass of floating object, have different ID than other instances'
 	//has different speed 
+	
+	/**
+	*Trash : a constructor of Trash
+	*@param String s: the name of this Trash
+	*@param int x: the x-position of this Trash
+	*@param int y: the y-position of this Trash
+	*@return Trash : Construct a new Trash
+	*/
 	public Trash(String n, int x, int y) {
 		super(n, x, y);
 		// TODO Auto-generated constructor stub
 		this.id = "Litter";
 	}
+	
+	/**
+	*Trash : a constructor of Trash
+	*@param String s: the name of this Trash
+	*@param int x: the x-position of this Trash
+	*@param int y: the y-position of this Trash
+	*@param int spee: the speed of this Trash
+	*@param int sco: the score of this Trash
+	*@param int due: the duration of this Trash
+	*@return Trash : Construct a new Trash
+	*/
 	public Trash(String n, int x, int y,int spee,int sco, int due) {
 		super(n, x, y,spee,sco,due);
 		// TODO Auto-generated constructor stub
 		this.id = "Litter";
 	}
 	
+	/**
+	*Trash : a constructor of Trash
+	*@param FloatingObjs f: a given FloatingObjs(should be Trash)
+	*@return Trash : clone a new Trash
+	*/
 	public Trash(FloatingObjs f) {
 		// TODO Auto-generated constructor stub
 		super(f);
 		this.id = "Litter";
 	}
 
+	/**
+	*move : update the location of this Trash
+	*@param void : nothing
+	*@return void : update the location of this Trash 
+	*/
 	public void move(){
 		yloc += yInc * speed;
 		yloc = Math.floorMod(yloc,1080);
