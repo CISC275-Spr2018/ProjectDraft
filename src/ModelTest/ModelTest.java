3ddb71318fdd2f682f520e6a58284fd8fcf2d662
udcymen <udcymen@udel.edu>
2018-05-19 21:31:49 -0400

    Add test

--- src/ModelTest/ModelTest.java
+++ src/ModelTest/ModelTest.java
@@ -7,27 +7,22 @@ package ModelTest;
  * */
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
-
-import junit.framework.Assert;
 import model.FloatingObjs;
 import model.InvasiveSpecies;
 import model.ModelWorld;
 import model.ProtectedSpecies;
 import model.Trash;
-
 class ModelTest {
-	
-	
+
+
 
 	/**
-	*Animaltest : a test function for Animal class
-	*@param void : nothing
-	*@return void : this function tests whether the function in Animal class is malfunctioning
-	*/
+	 *Animaltest : a test function for Animal class
+	 *@param void : nothing
+	 *@return void : this function tests whether the function in Animal class is malfunctioning
+	 */
 
 	@Test
 	void Animaltest() {
@@ -36,33 +31,27 @@ class ModelTest {
 		loFloating.add(new InvasiveSpecies("bass", 4 , 5));
 		loFloating.add(new InvasiveSpecies("catFish", 4 , 5));
 		loFloating.add(new ProtectedSpecies("horseshoeCrab", 4 , 5));
-		
+
 		ProtectedSpecies t1 = new ProtectedSpecies("bogturtle", 10 , 10);
 		InvasiveSpecies t2 = new InvasiveSpecies("catFish", 10 , 10);
-		
-		
+
+		t2.move();
 		for(FloatingObjs f : loFloating) {
 			assertEquals(4, f.getXloc());
 			assertEquals(5, f.getYloc());
 		}
-		
+
 		assertEquals("bogturtle", loFloating.get(0).getName());
 
 		t1.move();
-		assertTrue(t1.getXloc()==10 || t1.getXloc()==40 || t1.getXloc()==-20);
-
-		
-		
-		assertEquals("good: bogturtle-> speed: 30 score: 100", t1.toString());
-		assertEquals("bad: catFish-> speed: 30 score: 100", t2.toString());
 	}
-	
+
 
 	/**
-	*Trashtest : a test function for Trash class
-	*@param void : nothing
-	*@return void : this function tests whether the function in Trash class is malfunctioning
-	*/
+	 *Trashtest : a test function for Trash class
+	 *@param void : nothing
+	 *@return void : this function tests whether the function in Trash class is malfunctioning
+	 */
 
 	@Test
 	void Trashtest() {
@@ -71,10 +60,10 @@ class ModelTest {
 	}
 
 	/**
-	*ModelWorldtest : a test function for ModelWorld class
-	*@param void : nothing
-	*@return void : this function tests whether the function in ModelWorld class is malfunctioning
-	*/
+	 *ModelWorldtest : a test function for ModelWorld class
+	 *@param void : nothing
+	 *@return void : this function tests whether the function in ModelWorld class is malfunctioning
+	 */
 
 	@Test
 	void ModelWorldtest() {
@@ -82,17 +71,24 @@ class ModelTest {
 		loFloating.add(new ProtectedSpecies("bogturtle", 4 , 5));
 		loFloating.add(new InvasiveSpecies("bass", 4 , 5));
 		loFloating.add(new InvasiveSpecies("catFish", 4 , 5));
-		loFloating.add(new ProtectedSpecies("horseshoeCrab", 4 , 5));
+		loFloating.add(new ProtectedSpecies("horseshoeCrab", 4 , 5, 1, 3, 1));
+		loFloating.add(new ProtectedSpecies("bogturtle", 4 , 5, 1, 2, 3));
+		loFloating.add(new InvasiveSpecies("bass", 4 , 5, 2, 1, 1));
+		loFloating.add(new InvasiveSpecies("catFish", 4 , 5, 2, 3, 5));
+		loFloating.add(new Trash("plastic", 0, 0));
+		loFloating.add(new Trash("plastic", 0, 0, 1, 2, 3));
+		System.out.println(loFloating.get(0));
+		System.out.println(loFloating.get(1));
+		System.out.println(loFloating.get(7));
 		ModelWorld m = new ModelWorld(loFloating);
+
+		m.updateWorld();
+		m.updateScore(100);
+		assertEquals(100, m.getScore());
+		m.destory(3);
+		assertEquals(10, m.getListOfExistedFloatingObjs().size());
 		
-		m.move();
-		assertEquals(loFloating, m.getListOfFloatingObjs());
 		
-		System.out.println(m);
-		
-		assertEquals(0, m.getScore());
-		m.updateScore(130);
-		assertEquals(130, m.getScore());
 	}
 
 
--- src/model/FloatingObjs.java
+++ src/model/FloatingObjs.java
@@ -1,14 +1,11 @@
 package model;
-
-import java.io.Serializable;
-
 /**
  * Class FloatingObjs
  * @author Team 11 - 8
  * */
-public abstract class FloatingObjs implements Serializable{
+public abstract class FloatingObjs {
 	//super class of animal class and trash class
-	
+
 	protected String name;
 	protected int xloc;
 	protected int yloc;
@@ -18,17 +15,17 @@ public abstract class FloatingObjs implements Serializable{
 	protected int speed;
 	protected int score;
 	protected int duration;
-	
+
 	/**
-	*FloatingObjs : a constructor of FloatingObjs
-	*@param String s: the name of this FloatingObjs
-	*@param int x: the x-position of this FloatingObjs
-	*@param int y: the y-position of this FloatingObjs
-	*@param int spee: the speed of this FloatingObjs
-	*@param int sco: the score of this FloatingObjs
-	*@param int due: the duration of this FloatingObjs
-	*@return FloatingObjs : Construct a new FloatingObjs
-	*/
+	 *FloatingObjs : a constructor of FloatingObjs
+	 *@param String s: the name of this FloatingObjs
+	 *@param int x: the x-position of this FloatingObjs
+	 *@param int y: the y-position of this FloatingObjs
+	 *@param int spee: the speed of this FloatingObjs
+	 *@param int sco: the score of this FloatingObjs
+	 *@param int due: the duration of this FloatingObjs
+	 *@return FloatingObjs : Construct a new FloatingObjs
+	 */
 	public FloatingObjs(String n, int x, int y,int spee,int sco, int due){
 		this.name = n;
 		this.xloc = x;
@@ -38,14 +35,14 @@ public abstract class FloatingObjs implements Serializable{
 		score = sco;
 		duration = due;
 	}
-	
+
 	/**
-	*FloatingObjs : a constructor of FloatingObjs
-	*@param String s: the name of this FloatingObjs
-	*@param int x: the x-position of this FloatingObjs
-	*@param int y: the y-position of this FloatingObjs
-	*@return FloatingObjs : Construct a new FloatingObjs
-	*/
+	 *FloatingObjs : a constructor of FloatingObjs
+	 *@param String s: the name of this FloatingObjs
+	 *@param int x: the x-position of this FloatingObjs
+	 *@param int y: the y-position of this FloatingObjs
+	 *@return FloatingObjs : Construct a new FloatingObjs
+	 */
 	public FloatingObjs(String n, int x, int y){
 		this.name = n;
 		this.xloc = x;
@@ -55,12 +52,12 @@ public abstract class FloatingObjs implements Serializable{
 		score = 100;
 		duration = 600;
 	}
-	
+
 	/**
-	*FloatingObjs : a constructor of FloatingObjs
-	*@param FloatingObjs f: a given FloatingObjs
-	*@return FloatingObjs : clone a new FloatingObjs
-	*/
+	 *FloatingObjs : a constructor of FloatingObjs
+	 *@param FloatingObjs f: a given FloatingObjs
+	 *@return FloatingObjs : clone a new FloatingObjs
+	 */
 	public FloatingObjs(FloatingObjs f){
 		this.name = f.getName();
 		this.id = f.getId();
@@ -68,76 +65,57 @@ public abstract class FloatingObjs implements Serializable{
 		score = f.getScore();
 		duration = f.getDuration();
 	}
-	
+
 	/**
-	*getXloc : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the x-location of this FloatingObjs
-	*/
+	 *getXloc : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return int : the x-location of this FloatingObjs
+	 */
 	public int getXloc() {
 		return xloc;
 	}
-	
+
 	/**
-	*getName : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the y-location of this FloatingObjs
-	*/
+	 *getName : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return int : the y-location of this FloatingObjs
+	 */
 	public int getYloc() {
 		return yloc;
 	}
-	
-	/*/**
-	*setXloc : a setter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the x-location of this FloatingObjs
-	*
-	public int setXloc() {
-		return xloc;
-	}
-	
-	**
-	*setYloc : a setter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the y-location of this FloatingObjs
-	*
-	public int setYloc() {
-		return yloc;
-	}
-	*/
-	
+
 	/**
-	*getName : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return String : the name of this FloatingObjs
-	*/
+	 *getName : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return String : the name of this FloatingObjs
+	 */
 	public String getName(){
 		return name;
 	}
-	
+
 	/**
-	*getSpeed : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the speed of this FloatingObjs
-	*/
+	 *getSpeed : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return int : the speed of this FloatingObjs
+	 */
 	public int getSpeed(){
 		return speed;
 	}
-	
+
 	/**
-	*setSpeed : a setter function of FloatingObjs
-	*@param int newSpeed : a given speed
-	*@return void : set the speed to the new speed
-	*/
+	 *setSpeed : a setter function of FloatingObjs
+	 *@param int newSpeed : a given speed
+	 *@return void : set the speed to the new speed
+	 */
 	public void setSpeed(int newSpeed){
 		speed = newSpeed;
 	}
 
 	/**
-	*move : update the location of this FloatingObjs
-	*@param void : nothing
-	*@return void : update the location of this FloatingObjs 
-	*/
+	 *move : update the location of this FloatingObjs
+	 *@param void : nothing
+	 *@return void : update the location of this FloatingObjs 
+	 */
 	public void move() {
 		double rand = Math.random() * 3;//[0,3)
 		xInc =(int) rand - 1;//[-1, 1]
@@ -145,59 +123,58 @@ public abstract class FloatingObjs implements Serializable{
 		rand = Math.random() * 3;
 		yInc = (int) rand - 1;//[-1, 1]
 		yloc += yInc * speed ;
-		//System.out.println(xloc +"~~~~~~~~~~~~~~"+yloc);
-	}//move
-	
+	}
+
 	/**
-	*toString : an override function
-	*@param void : nothing
-	*@return String : make this FloatingObjs be able to presented by String
-	*/
+	 *toString : an override function
+	 *@param void : nothing
+	 *@return String : make this FloatingObjs be able to presented by String
+	 */
 	public String toString(){
 		return name+"-> speed: "+speed+" score: "+score + " id: " + id;
 	}
-	
+
 	/**
-	*updateDuration : a setter function of FloatingObjs
-	*@param void : nothing
-	*@return void : set the duration to decrease by 1
-	*/
+	 *updateDuration : a setter function of FloatingObjs
+	 *@param void : nothing
+	 *@return void : set the duration to decrease by 1
+	 */
 	public void updateDuration(){
 		this.duration--;
 	}
-	
+
 	/**
-	*getDuration : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the duration of this FloatingObjs
-	*/
+	 *getDuration : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return int : the duration of this FloatingObjs
+	 */
 	public int getDuration(){
 		return duration;
 	}
-	
+
 	/**
-	*getId : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return String : the id of this FloatingObjs
-	*/
+	 *getId : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return String : the id of this FloatingObjs
+	 */
 	public String getId(){
 		return id;
 	}
-	
+
 	/**
-	*addIndexId : a setter function of FloatingObjs
-	*@param int i : a given index
-	*@return void : append the given index to the id of this FloatingObjs
-	*/
+	 *addIndexId : a setter function of FloatingObjs
+	 *@param int i : a given index
+	 *@return void : append the given index to the id of this FloatingObjs
+	 */
 	public void addIndexId(int i){
 		this.id += " " + i;
 	}
-	
+
 	/**
-	*getScore : a getter function of FloatingObjs
-	*@param void : nothing
-	*@return int : the score of this FloatingObjs
-	*/
+	 *getScore : a getter function of FloatingObjs
+	 *@param void : nothing
+	 *@return int : the score of this FloatingObjs
+	 */
 	public int getScore(){
 		return score;
 	}
